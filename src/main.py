534a9ff2ec4a41b1d91caa2472dd77a6517bf19a
Sevelantis <hamer223@o2.pl>
2023-03-22 09:26:45 +0000

    Fixed linting

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     logging.info('Now I can access the variables from the app\'s code!')
-    logging.info(f'''
-                 ACTIVEMQ_HOST -> {config.ACTIVEMQ_HOST},
-                 ACTIVEMQ_PORT -> {config.ACTIVEMQ_PORT},
-                 ''')
+    logging.info(
+        f'''
+        ACTIVEMQ_HOST -> {config.ACTIVEMQ_HOST},
+        ACTIVEMQ_PORT -> {config.ACTIVEMQ_PORT},
+        ''')
 
 if __name__ == '__main__':
     main()
