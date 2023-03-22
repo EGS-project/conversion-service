12533e1cb677db4b9d1ded280f54867b67d97395
Sevelantis <hamer223@o2.pl>
2023-03-22 09:27:10 +0000

    Fixed linting

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 '''THIS FILE IS SAFE TO PUSH'''
 
+
 from dotenv import find_dotenv, load_dotenv
 from os import environ as env
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
+if ENV_FILE := find_dotenv():
     load_dotenv(ENV_FILE)
 
 # BROKER VARIABLES
